62acc43b72e8824eef1773fa7651db6d9f02a73b
Tayyab Aman <95743660+TAYYAB-IT@users.noreply.github.com>
2022-02-02 11:53:43 +0500

    Add files via upload

--- Routes/user_routes.js
+++ Routes/user_routes.js
@@ -3,5 +3,5 @@ const user=require('../Controllers/user_controller');
 const router=require('express').Router();
 router.post('/signup',user.signup);
 router.post('/signin',user.signin);
-router.get('/user/greeter',Auth.token_verify,Auth.checkrole("User"),user.greeter)
+router.get('/user/greeter',Auth.token_verify,Auth.checkrole(["User","Admin","Employee"]),user.greeter)
 module.exports=router;
